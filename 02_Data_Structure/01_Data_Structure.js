41a9f1259fe8fdc847e46ed8fcc9c7cb6b021fa4
Lee YoungCheol <ofetwo@gmail.com>
2020-10-23 21:10:22 +0900

    Upload 02_Data Structue

---
+++ 02_Data_Structure/01_Data_Structure.js
@@ -0,0 +1,33 @@
+/**
+ * Data Structure
+ */
+
+/*
+
+  1. Stack
+    가져오기: O(n)
+    추가하기: O(1)
+    삭제하기: O(1)
+
+  2. Queue
+    가져오기: O(n)
+    추가하기: O(1)
+    삭제하기: O(1)
+
+  3. Linked List
+    가져오기: O(n)
+    추가하기: O(1)
+    삭제하기: O(1)
+
+  4. Hash Table
+    가져오기: O(1)
+    추가하기: O(1)
+    삭제하기: O(1)
+
+  5. Graph
+
+  6. Tree
+
+  7. Binary Search Tree
+
+*/

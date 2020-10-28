476732269e772edb783e68670e01399ec84be078
Lee YoungCheol <ofetwo@gmail.com>
2020-10-28 23:48:04 +0900

    Update 02_Data Structure

--- 02_Data_Structure/01_Data_Structure.js
+++ 02_Data_Structure/01_Data_Structure.js
@@ -4,30 +4,42 @@
 
 /*
 
-  1. Stack
-    가져오기: O(n)
-    추가하기: O(1)
-    삭제하기: O(1)
-
-  2. Queue
-    가져오기: O(n)
-    추가하기: O(1)
-    삭제하기: O(1)
-
-  3. Linked List
-    가져오기: O(n)
-    추가하기: O(1)
-    삭제하기: O(1)
-
-  4. Hash Table
+  1. Array
     가져오기: O(1)
+    추가하기: O(n)
+    삭제하기: O(n)
+
+  2. Stack
+    가져오기: O(n)
     추가하기: O(1)
     삭제하기: O(1)
 
-  5. Graph
+  3. Queue
+    가져오기: O(n)
+    추가하기: O(1)
+    삭제하기: O(1)
 
-  6. Tree
+  4. Linked List
+    가져오기: O(n)
+    추가하기: O(1)
+    삭제하기: O(1)
+
+  5. Hash Table
+            <average>   <average>
+    가져오기:    O(1)        O(n)
+    추가하기:    O(1)        O(n)
+    삭제하기:    O(1)        O(n)
+
+  6. Graph
+
+    가져오기: O(V)
+    추가하기: O(1)
+    삭제하기: O(1)
 
   7. Binary Search Tree
+            <average>
+    가져오기: O(log(n))
+    추가하기: O(log(n))
+    삭제하기: O(log(n))
 
 */

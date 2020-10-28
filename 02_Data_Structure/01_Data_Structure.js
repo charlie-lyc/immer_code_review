4cd1d7a7ae3b9c36b89b36de596b6988caffbdd6
Lee YoungCheol <ofetwo@gmail.com>
2020-10-29 00:03:09 +0900

    Update 02_Data Structure

--- 02_Data_Structure/01_Data_Structure.js
+++ 02_Data_Structure/01_Data_Structure.js
@@ -2,7 +2,7 @@
  * Data Structure
  */
 
-/*
+/* Time Complexity
 
   1. Array
     가져오기: O(1)
@@ -19,27 +19,34 @@
     추가하기: O(1)
     삭제하기: O(1)
 
-  4. Linked List
+  4. Linked List: Single and Double
     가져오기: O(n)
     추가하기: O(1)
     삭제하기: O(1)
 
   5. Hash Table
-            <average>   <average>
-    가져오기:    O(1)        O(n)
-    추가하기:    O(1)        O(n)
-    삭제하기:    O(1)        O(n)
+            <average>  <worst>
+    가져오기:    O(1)      O(n)
+    추가하기:    O(1)      O(n)
+    삭제하기:    O(1)      O(n)
 
   6. Graph
+    (1) Adjacency List : 빈번한 추가, 삭제에 유리
+      조회하기:   O(V)
+              [Vertex]  [Edge]
+      추가하기:   O(1)     O(1)
+      삭제하기:   O(V+E)   O(E)
 
-    가져오기: O(V)
-    추가하기: O(1)
-    삭제하기: O(1)
+    (2) Adjacency Matrix : 조회에 유리
+      조회하기:    O(1)
+              [Vertex]  [Edge]
+      추가하기:   O(V*V)   O(1)
+      삭제하기:   O(V*V)   O(1)
 
   7. Binary Search Tree
-            <average>
-    가져오기: O(log(n))
-    추가하기: O(log(n))
-    삭제하기: O(log(n))
+              <average>  <worst>
+      가져오기:  O(log(n))   O(n)
+      추가하기:  O(log(n))   O(n)
+      삭제하기:  O(log(n))   O(n)
 
 */
